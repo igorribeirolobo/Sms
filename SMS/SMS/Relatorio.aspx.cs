6efd882c6c2bef9f9dfbf31fdb63c26441f937e5
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2016-04-14 21:53:18 -0300

    novo relatorio
    relatorio data + setor

--- SMS/SMS/Relatorio.aspx.cs
+++ SMS/SMS/Relatorio.aspx.cs
@@ -208,6 +208,54 @@ namespace SMS
 
         protected void btnGerar2_Click(object sender, EventArgs e)
         {
+            tblrelatorio1.Visible = true;
+            if (tblrelatorio.Visible == true)
+            {
+                tblrelatorio.Visible = false;
+            }
+            TableRow tb = new TableRow();
+            tblrelatorio1.Rows.Add(tb);
+            TableCell cell = new TableCell();
+            TableCell cell1 = new TableCell();
+            TableCell cell2 = new TableCell();
+            cell.Text = "Usuário";
+            cell1.Text = "Quantidade de Sms";
+            cell2.Text = "Data";
+            tb.Cells.Add(cell);
+            tb.Cells.Add(cell1);
+            tb.Cells.Add(cell2);
+            string dataini = cdini.SelectedDate.ToString();
+            string[] inicio = dataini.Split(" ".ToCharArray());
+            string[] inidata = inicio[0].Split("/".ToCharArray());
+            string nova_dataini = inidata[2] + "/" + inidata[1] + "/" + inidata[0] + " " + inicio[1];
+            string datafim = cdfim.SelectedDate.ToString();
+            string[] fim = datafim.Split(" ".ToCharArray());
+            string[] fimdata = fim[0].Split("/".ToCharArray());
+            string nova_datafim = fimdata[2] + "/" + fimdata[1] + "/" + fimdata[0] + " " + fim[1];
+            string sql = "SELECT * FROM login WHERE idLogin=" + ddSetor.SelectedValue;
+            string sql1 = "SELECT * FROM tb_contagem WHERE id_user=" + ddSetor.SelectedValue + " AND data >=" + dataini + " AND data <=" + datafim;
+            MySqlDataReader reader = this.banco.leitor(sql);
+            MySqlDataReader ler = this.banco.leitor(sql1);
+            int soma = 0;
+            while(reader.Read())
+            {  
+                while(ler.Read())
+                {
+                    TableRow tb1 = new TableRow();
+                    tblrelatorio.Rows.Add(tb1);
+                    TableCell coluna = new TableCell();
+                    TableCell coluna1 = new TableCell();
+                    TableCell coluna2 = new TableCell();
+                    soma += Convert.ToInt16(reader["cont"].ToString());
+                    coluna.Text = ler["Nome"].ToString();
+                    coluna1.Text = reader["cont"].ToString();
+                    coluna2.Text = reader["data"].ToString();
+                    tb1.Cells.Add(coluna);
+                    tb1.Cells.Add(coluna1);
+                    tb1.Cells.Add(coluna2);
+                }
+                
+            }
 
         }
     }
