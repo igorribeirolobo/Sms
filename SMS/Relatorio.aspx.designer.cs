1a792517d94be9a9cf1dff714b25f3ec9a4f80a2
admin <iguim37@hotmail.com>
2016-04-15 07:55:57 -0300

    label
    retirada do label

--- SMS/Relatorio.aspx.designer.cs
+++ SMS/Relatorio.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace SMS {
         protected global::System.Web.UI.WebControls.Button btnGerar;
         
         /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
         /// lbldataini control.
         /// </summary>
         /// <remarks>
